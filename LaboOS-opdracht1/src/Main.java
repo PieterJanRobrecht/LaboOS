92fbf73cb0e5bf8f1042d39c2bb0db28ea7afafe
Pieter-Jan Robrecht <robrechtpieterjan@gmail.com>
2016-03-03 16:00:51 +0100

    pad aangepast
    pad is aangepast zodanig dat deze universeel is

--- LaboOS-opdracht1/src/Main.java
+++ LaboOS-opdracht1/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			File file = new File("C:\\Users\\Pieter-Jan\\Documents\\workspace\\OS2_Opdracht1\\processen10000.xml");
+			File file = new File("processen10000.xml");
 //			File file = new File("C:\\Users\\Pieter-Jan\\Documents\\workspace\\OS2_Opdracht1\\test.xml");
 			JAXBContext jaxbContext = JAXBContext.newInstance(Processlist.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
