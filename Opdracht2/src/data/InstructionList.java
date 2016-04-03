a029e83e78b082e2dc99b01185d0492fbdce97d7
Pieter-Jan Robrecht <robrechtpieterjan@gmail.com>
2016-04-03 14:38:22 +0200

    Simpele unmarshal

---
+++ Opdracht2/src/data/InstructionList.java
@@ -0,0 +1,37 @@
+package data;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "instructionlist")
+public class InstructionList {
+
+	@XmlElement(name = "instruction")
+	private List<Instruction> instructies;
+
+	public InstructionList() {
+		super();
+	}
+
+	public List<Instruction> getInstructies() {
+		return instructies;
+	}
+
+	public void setInstructies(List<Instruction> instructies) {
+		this.instructies = instructies;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for(Instruction i : instructies){
+			sb.append(i.toString());
+		}
+		return sb.toString();
+	}
+}
--- Opdracht2/src/main/Main.java
+++ Opdracht2/src/main/Main.java
@@ -1,9 +1,33 @@
 package main;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import data.InstructionList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		File file = new File("Instructions_30_3.xml");
+
+		JAXBContext jaxbContext;
+		InstructionList lijst = null;
+		try {
+			jaxbContext = JAXBContext.newInstance(InstructionList.class);
+			
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+			lijst = (InstructionList) jaxbUnmarshaller.unmarshal(file);
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(lijst);
 
 	}
 
