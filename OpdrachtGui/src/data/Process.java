b8b4a80b6c0c4f6d32e09d0973a702a7fb5ba932
Piet Vanhaute <Piet Vanhaute>
2016-04-07 22:01:04 +0200

    Main verwijderen en Enkele Processchanges

--- OpdrachtGui/src/data/Process.java
+++ OpdrachtGui/src/data/Process.java
@@ -7,6 +7,24 @@ public class Process {
 	public Process(){
 		
 	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public PageTable getPagetable() {
+		return pagetable;
+	}
+
+	public void setPagetable(PageTable pagetable) {
+		this.pagetable = pagetable;
+	}
+	
+	
 	
 	
 }
