57ec05c00bcbc88ece0e6fddfbf32534bf43034a
Pieter-Jan Robrecht <robrechtpieterjan@gmail.com>
2016-04-07 12:05:39 +0200

    Simpele knop gemaakt

--- OpdrachtGui/src/controllers/Controller.java
+++ OpdrachtGui/src/controllers/Controller.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
 	
 	int i = 0;
-	
+
+	private Button eenStap;
+
 	public void eenStapClicked(){
 		System.out.println(i++);
 	}
