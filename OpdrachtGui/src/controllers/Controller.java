3ddfcde6823c7c0dac50c199ed2eb9bd372e1857
Pieter-Jan Robrecht <robrechtpieterjan@gmail.com>
2016-04-07 15:46:32 +0200

    Gui

--- OpdrachtGui/src/controllers/Controller.java
+++ OpdrachtGui/src/controllers/Controller.java
@@ -6,10 +6,19 @@ import javafx.scene.control.Button;
 public class Controller {
 	
 	int i = 0;
-
+	
+	@FXML
 	private Button eenStap;
 
 	public void eenStapClicked(){
 		System.out.println(i++);
+	}
+
+	public Button getEenStap() {
+		return eenStap;
+	}
+
+	public void setEenStap(Button eenStap) {
+		this.eenStap = eenStap;
 	}
 }
